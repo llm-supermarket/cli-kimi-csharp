38677b34573d1b1baa62135e42d1d5a08d54f6d3
Chris S. <54912+yetanotherchris@users.noreply.github.com>
2026-06-25 21:44:43 +0100

    chore: update package manifests to v1.0.0

--- Formula/rclone-encrypt-test-kimi-csharp.rb
+++ Formula/rclone-encrypt-test-kimi-csharp.rb
@@ -6,20 +6,20 @@ class RcloneEncryptTestKimiCsharp < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/llm-supermarket/rclone-encrypt-test-kimi-csharp/releases/download/v1.0.0/rclone-encrypt-test-kimi-csharp-darwin-arm64.tar.gz"
-      sha256 "PLACEHOLDER"
+      sha256 "1230e759f25db5fecfdb11d00a164b300aa0682d2f9657330b2420eb8a91d90c"
     else
       url "https://github.com/llm-supermarket/rclone-encrypt-test-kimi-csharp/releases/download/v1.0.0/rclone-encrypt-test-kimi-csharp-darwin-amd64.tar.gz"
-      sha256 "PLACEHOLDER"
+      sha256 "855095a5584c2c2cdbd7cbc82d600aea196a6346019f702a8c4f2a9e44221831"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/llm-supermarket/rclone-encrypt-test-kimi-csharp/releases/download/v1.0.0/rclone-encrypt-test-kimi-csharp-linux-arm64.tar.gz"
-      sha256 "PLACEHOLDER"
+      sha256 "cd1820f5b1af09c401d06d400936e7026602d3709e7b9168fadf87007b862868"
     else
       url "https://github.com/llm-supermarket/rclone-encrypt-test-kimi-csharp/releases/download/v1.0.0/rclone-encrypt-test-kimi-csharp-linux-amd64.tar.gz"
-      sha256 "PLACEHOLDER"
+      sha256 "c063a87e2c68d314122fc4e58f4195b4458711e36d1c28a26925cac90ffed683"
     end
   end
 
